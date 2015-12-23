02d91f1c7c8c11711606dfe9cb7eab218f2019c1
zhuliyi10 <2280835569@qq.com>
2015-12-24 02:28:29 +0800

    添加上次commit漏的文件

---
+++ app/src/main/java/com/zhuliyi/app/wishwall/config/Configuration.java
@@ -0,0 +1,13 @@
+package com.zhuliyi.app.wishwall.config;
+
+/**
+ * 软件常用的配置放在此类中
+ * Created by zhuliyi on 2015/12/23.
+ */
+public class Configuration {
+    //设置的参数
+    public static class Equipment{
+        public static int SCREEN_WIDTH;
+        public static int SCREEN_HEIGHT;
+    }
+}
